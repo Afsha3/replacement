c7c6baf16cfa9e848aa2917cd896e08083d633bf
ADMIN <ADMIN@192.168.0.3>
2020-07-29 15:17:44 +0530

    replacing

---
+++ method/src/method/Replaceexample.java
@@ -0,0 +1,8 @@
+package method;
+
+public class Replaceexample {
+	public static void main(String args[]){ 
+		String s1="hello how are you"; 
+		String replaceString=s1.replace('h','n'); 
+		System.out.println(replaceString); }}
+
